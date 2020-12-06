1e79634971183b1c29c653e1969bf4a9cf2d1445
Quang Phan <contact.quangphan@gmail.com>
2020-12-06 19:08:43 +0700

    🔨refactor(models.Session): split JWTClaims

--- src/models/Session.ts
+++ src/models/Session.ts
@@ -1,16 +1,17 @@
 import type { Role, Scope } from "/services/graphql/generated/types";
 
+export interface JWTClaims {
+  role: Role,
+  scopes: Scope[],
+  id: string,
+  name: string,
+  exp: number,  // expiration time
+  aud: string,  // audience
+  iss: string,  // issuer
+  iat: number,  // issued at
+}
 export default interface Session {
-  claims: {
-    role: Role,
-    scopes: Scope[],
-    id: string,
-    name: string,
-    exp: number,  // expiration time
-    aud: string,  // audience
-    iss: string,  // issuer
-    iat: number,  // issued at
-  }
+  claims: JWTClaims
 }
 
 export {};
